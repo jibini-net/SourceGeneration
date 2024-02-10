5e94ec327bab2d80e27d2b486644d45c7c20aede
Zach Goethel <13502112+zgoethel@users.noreply.github.com>
2024-02-09 23:55:06 -0600

    Consolidate anchor headers

--- TestApp/Extensions/ServiceCollectionExtensions.cs
+++ TestApp/Extensions/ServiceCollectionExtensions.cs
@@ -22,6 +22,7 @@ public static class ServiceCollectionExtensions
         services.AddHostView<HostBase.Default>();
         services.AddLayoutView<LayoutBase.Default>();
         services.AddSeoExcludeView<SeoExcludeBase.Default>();
+        services.AddAnchorHeadingView<AnchorHeadingBase.Default>();
         services.AddCascadingStateView<CascadingState>();
         services.AddHomePageView<HomePageBase.Default>();
         services.AddHomeView<HomeBase.Default>();
