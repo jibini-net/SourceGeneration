29a0f0984221e61f818abdce11846055b6a75dbc
Zach Goethel <13502112+zgoethel@users.noreply.github.com>
2024-02-28 23:10:59 -0600

    Delete TestApp.Web directory

--- TestApp.Web/wwwroot/js/site.js
+++
@@ -1,9 +0,0 @@
-var Layout = {
-    init: () => {
-        setInterval(Layout.actions.toggleBlinkCursor, 1000);
-    },
-    actions: {
-        toggleBlinkCursor: () => $(".blink-cursor").toggleClass("d-none")
-    }
-};
-$(Layout.init);
