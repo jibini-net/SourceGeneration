e1317b2cfd2c3382f11a01154c1779408a0c71c4
Zach Goethel <13502112+zgoethel@users.noreply.github.com>
2024-08-10 19:42:53 -0500

    Fix drawer scroll on close

--- TestApp.Web/wwwroot/js/Drawer.js
+++ TestApp.Web/wwwroot/js/Drawer.js
@@ -1,10 +1,15 @@
 var Drawer = {
     toggle: async (el) => {
         var startScroll = document.documentElement.scrollTop;
+        var startDocHeight = document.documentElement.scrollHeight;
         var startState = $(el).data("state");
-        await dispatch(el, 'Toggle');
-        if (startState === "closed") {
-            window.scrollTo({ top: startScroll, left: 0, behavior: "instant" });
+
+        await dispatch(el, "Toggle");
+
+        if (startState === "open") {
+            var offsetFromHeight = startScroll - startDocHeight;
+            startScroll = document.documentElement.scrollHeight + offsetFromHeight;
         }
+        window.scrollTo({ top: startScroll, left: 0, behavior: "instant" });
     }
 };
